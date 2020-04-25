bbbea8fb6c58364d6b898a70110cc5b5f1f35b2d
ansheng <535819981qq.com>
2020-04-25 09:27:41 +0800

    add feature_gan.py

--- triplet_sperate.py
+++ triplet_sperate.py
@@ -1,3 +1,5 @@
+#这个代码实现了如何让REID的market1501数据库，按照triplet loss的形式，形成anchor positive negative 对，形成N×K的mini batch送入网络，N代表行人ID的数量，K代表每个ID具有的图片数量 对这个代码的博客讲解地址为：  https://blog.csdn.net/t20134297/article/details/105712627
+
 import os.path as osp
 import glob
 import re
